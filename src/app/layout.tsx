e5a35083f0183ee0f72b57064227d5b92f5f908a
abdusSamadJIMS <abdussamadjims@gmail.com>
2024-08-19 23:39:38 +0530

    changes in manifest file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,13 @@ export const metadata: Metadata = {
     }
   },
   applicationName: siteConfig.name,
+  icons: {
+    icon: ['/favicon.ico?v=1'],
+    apple: ['apple-touch-icon.png?v=4'],
+    shortcut: ['apple-touch-icon.png'],
+  },
+  manifest: "/site.webmanifest"
+
 };
 
 
